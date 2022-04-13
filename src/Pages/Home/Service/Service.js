8ccc2f29bf594149e46a546f780a23ea399d8a0d
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-04-13 14:30:51 +0600

    vanila css responsive

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Service.css'
+
+const Service = ({service}) => {
+    const {name,description,price,img} = service;
+    return (
+        <div className='service-container'>
+           
+          <img src={img} alt="" />
+           <h3>{name}</h3>
+           <p>Price: {price}</p> 
+           <p>{description}</p>
+          
+        </div>
+    );
+};
+
+export default Service;
