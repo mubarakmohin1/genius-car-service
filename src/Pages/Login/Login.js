37d7cf8ddb6d986b0f62634b3c230b7595918a52
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-04-14 12:47:51 +0600

    login and registerd compononet created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+    const navigate = useNavigate();
+
+    const handleSubmitButton = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+
+    }
+    const navigateRegister = event => {
+        navigate('/register')
+    }
+
+
     return (
-        <div>
-            
+        <div className='container mx-auto w-50'>
+            <h2 className='text-center text-primary mt-2'>Please Login!!</h2>
+            <Form onSubmit={handleSubmitButton}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+            <p>New to Genius car? <Link to={'/register'} className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register!</Link></p>
         </div>
     );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = () => {
+        navigate("/login")
+    }
+    const handleSubmitRegister = event =>{
+        event.preventDefault();
+    }
     return (
-        <div>
-            
+        <div className='container register-form'>
+            <h2 className='text-center text-primary mt-3'>Please Register!!</h2>
+            <form onSubmit={handleSubmitRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' /> <br />
+                <input type="email" name="email" id="" placeholder='Email Address' required /> <br />
+                <input type="Password" name="password" id="" placeholder='Password' required /> <br />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already Registered? <Link to={'/login'} className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login!</Link></p>
         </div>
     );
 };
